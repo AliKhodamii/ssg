6938ccd59e6c206b3325531f91807dc219126a54
Sed <s.alikhodami@gmail.com>
2025-05-19 16:08:21 +0330

    no need

--- backend/db.php
+++
@@ -1,8 +0,0 @@
-<?
-$dbname = "jjqioyps_ssg";
-$host = "localhost:3306";
-$user = "jjqioyps_ssg";
-$pass = "123456";
-
-$pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
